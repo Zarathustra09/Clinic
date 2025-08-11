efa04e3dee4e956c47b5e1b8f4efba05ea30c0ce
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2025-08-11 14:20:26 +0800

    Add peak hours analytics to the dashboard and update layout

--- Clinic/Controllers/HomeController.cs
+++ Clinic/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Clinic.Models;
 using Clinic.DataConnection;
 
@@ -13,12 +14,59 @@ public class HomeController : BaseController
     {
         _logger = logger;
     }
-    
-    // Rest of your methods remain the same...
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        // Get peak hours analytics data
+        var peakHoursData = await GetPeakHoursAnalytics();
+        ViewBag.PeakHoursData = peakHoursData;
+
         return View();
+    }
+
+    private async Task<object> GetPeakHoursAnalytics()
+    {
+        var appointments = await _context.Appointments
+            .Include(a => a.TimeSlot)
+            .Include(a => a.Branch)
+            .Where(a => a.IsApproved && a.TimeSlot != null)
+            .ToListAsync();
+
+        // Group by branch and hour
+        var peakHours = appointments
+            .GroupBy(a => new {
+                BranchId = a.BranchId,
+                BranchName = a.Branch.Name,
+                Hour = a.TimeSlot.StartTime.Hour
+            })
+            .Select(g => new {
+                BranchId = g.Key.BranchId,
+                BranchName = g.Key.BranchName,
+                Hour = g.Key.Hour,
+                AppointmentCount = g.Count()
+            })
+            .OrderBy(x => x.BranchName)
+            .ThenBy(x => x.Hour)
+            .ToList();
+
+        // Get branch totals
+        var branchTotals = appointments
+            .GroupBy(a => new { a.BranchId, a.Branch.Name })
+            .Select(g => new {
+                BranchId = g.Key.BranchId,
+                BranchName = g.Key.Name,
+                TotalAppointments = g.Count(),
+                PeakHour = g.GroupBy(a => a.TimeSlot.StartTime.Hour)
+                           .OrderByDescending(h => h.Count())
+                           .FirstOrDefault()?.Key ?? 0
+            })
+            .ToList();
+
+        return new {
+            PeakHours = peakHours,
+            BranchTotals = branchTotals,
+            TotalAppointments = appointments.Count
+        };
     }
 
     public IActionResult Privacy()
